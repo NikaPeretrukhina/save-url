8292adbee7651132da209dc046834283dd5ee394
NikaPeretrukhina <nika.peretrukhina@gmail.com>
2026-03-01 00:10:17 +0100

    database and other changes

--- index.js
+++ index.js
@@ -1,39 +1,46 @@
-let myLeads = []
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js"
+import {
+    getDatabase,
+    ref,
+    push,
+    onValue,
+    remove
+} from "https://www.gstatic.com/firebasejs/12.10.0/firebase-database.js"
+
+
+const firebaseConfig = {
+    databaseURL: "https://lead-tracker-app-c60dd-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app)
+const referenceInDB = ref(database, "leads")
+
+
+
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-const tabBtn = document.getElementById("tab-btn")
+
 const themeToggle = document.getElementById("theme-toggle")
 const htmlElement = document.documentElement
 
 
 
+themeToggle.addEventListener("click", function () {
+    let currentTheme = htmlElement.getAttribute('data-theme')
+    let newTheme = (currentTheme === 'dark') ? 'light' : 'dark'
 
+    htmlElement.setAttribute('data-theme', newTheme)
+    themeToggle.innerHTML = (newTheme === 'dark') ? "☀️" : "🌙"
 
-themeToggle.addEventListener("click", function(){
-let currentTheme = htmlElement.getAttribute('data-theme')
-let newTheme = (currentTheme === 'dark') ? 'light' : 'dark'
-
-htmlElement.setAttribute('data-theme', newTheme)
-themeToggle.innerHTML = (newTheme ==='dark') ? "☀️" : "🌙"
-
-localStorage.setItem('theme', newTheme)
+    localStorage.setItem('theme', newTheme)
 })
 
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
-}
 
-tabBtn.addEventListener("click", function(){    
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
-    })
-})
+
+
 
 function render(leads) {
     let listItems = ""
@@ -49,15 +56,22 @@ function render(leads) {
     ulEl.innerHTML = listItems
 }
 
-deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
+onValue(referenceInDB, function(snapshot){
+    const snapshotDoesExist = snapshot.exists()
+    if(snapshotDoesExist){
+        const snapshotValues = snapshot.val()
+        const leads = Object.values(snapshotValues)
+        render(leads)
+    }
 })
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
+deleteBtn.addEventListener("dblclick", function () {
+    remove(referenceInDB)
+    ulEl.innerHTML = ""
+})
+
+inputBtn.addEventListener("click", function () {
+    push(referenceInDB, inputEl.value)
     inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render(myLeads)
+
 })
